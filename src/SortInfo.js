ad13304c40a2e941ac7a1824094a233d1a3cd2c9
Manuel Bugallo <marvinody@gmail.com>
2019-11-19 16:44:51 -0500

    feat: updates complexity styling

--- src/SortInfo.js
+++ src/SortInfo.js
@@ -18,18 +18,31 @@ export default function SortInfo({ data }) {
   )
 }
 
-const Complexities = ({ best, average, worst }) => {
+
+const complexityClasses = {
+  '1': 'great',
+  'log(n)': 'good',
+  'n': 'ok',
+  'n*log(n)': 'meh',
+  'n^2': 'bad',
+}
+
+const Complexities = (props) => {
   return (
     <div className='sort-complexities'>
-      <div className='sort-complexities-header'>
-        <span>Best</span>
-        <span>Average</span>
-        <span>Worst</span>
-      </div>
-      <div className='sort-complexities-row'>
-        <span className='best'>{best}</span>
-        <span className='average'>{average}</span>
-        <span className='worst'>{worst}</span>
+      <div className='sort-complexities-table'>
+        <div className='sort-complexities-header'>
+          <span>Best</span>
+          <span>Average</span>
+          <span>Worst</span>
+        </div>
+        <div className='sort-complexities-row'>
+          {['best', 'average', 'worst'].map(w => {
+            const complexityCase = props[w]
+            const className = `${w} ${complexityClasses[complexityCase]}`
+            return <span className={className}>{complexityCase}</span>
+          })}
+        </div>
       </div>
     </div>
   )

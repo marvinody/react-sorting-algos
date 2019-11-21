a74932d19950c89dbcd3d97e0cc6d5f2729545d4
Manuel Bugallo <marvinody@gmail.com>
2019-11-21 16:38:15 -0500

    feat: makes navbar dynamically generated

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import sorts from './sorts'
+
 export default function NavBar() {
   return (
     <div>
       <div>
-        <Link to='/bubble-sort'>Bubble Sort</Link>
-        <Link to='/merge-sort'>Merge Sort</Link>
+        {Object.values(sorts).map(({ data: sort }) => (
+          <Link to={'/' + sort.slug} key={sort.slug}>{sort.name}</Link>
+        ))}
+
       </div>
     </div>
   )

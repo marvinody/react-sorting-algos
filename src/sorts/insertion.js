dba2f0cecde38fb5bf5b3ac0644c5345b169af20
Tal Luigi <luigi.t.agr@gmail.com>
2019-11-25 22:05:13 -0500

    feat: added insertion sort name, complexities, link, and desc in insertion.js

--- src/sorts/insertion.js
+++ src/sorts/insertion.js
@@ -4,16 +4,32 @@ function* insertion_sort(arr, { swap }) {
     let j = i;
     while (j > 0 && arr[j - 1] > arr[j]) {
       arr = swap(j, j - 1);
-      yield
+      yield;
       j = j - 1;
     }
     i = i + 1;
   }
-
 }
 
-export default insertion_sort
+const name = 'Insertion Sort';
+const slug = 'insertion-sort';
+
+const complexities = {
+  best: 'n',
+  average: 'n^2',
+  worst: 'n^2',
+};
+
+const link = 'https://en.wikipedia.org/wiki/Insertion_sort';
+
+const desc = `Insertion sort is a simple sorting algorithm that builds the final sorted array (or list) one item at a time. It is much less efficient on large lists than more advanced algorithms such as quicksort, heapsort, or merge sort.`;
 
 export const data = {
-  slug: 'insertion-sort'
-}
+  name,
+  slug,
+  complexities,
+  link,
+  desc,
+};
+
+export default insertion_sort;

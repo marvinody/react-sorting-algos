9ca078804760083b6d434b4520b66decc84931ef
Tal Luigi <luigi.t.agr@gmail.com>
2019-11-25 22:07:26 -0500

    chore: matched code order and style throughout all sorting algorithms

--- src/sorts/bubble.js
+++ src/sorts/bubble.js
@@ -4,34 +4,33 @@ function* bubble_sort(arr, { swap }) {
   while (swapped) {
     for (let i = 1; i < n; i++) {
       if (arr[i - 1] > arr[i]) {
-        arr = swap(i - 1, i)
+        arr = swap(i - 1, i);
         yield;
         swapped = true;
       }
     }
   }
-
 }
 
-const name = 'Bubble Sort'
-const slug = 'bubble-sort'
+const name = 'Bubble Sort';
+const slug = 'bubble-sort';
 
 const complexities = {
   best: 'n',
   average: 'n^2',
   worst: 'n^2',
-}
+};
 
-const link = 'https://en.wikipedia.org/wiki/Bubble_sort'
+const link = 'https://en.wikipedia.org/wiki/Bubble_sort';
 
-const desc = `A simple sorting algorithm that repeatedly steps through the list, compares adjacent elements and swaps them if they are in the wrong order. The pass through the list is repeated until the list is sorted. The pass through the list is repeated until the list is sorted. The algorithm, which is a comparison sort, is named for the way smaller or larger elements "bubble" to the top of the list. Although the algorithm is simple, it is too slow and impractical for most problems even when compared to insertion sort. Bubble sort can be practical if the input is in mostly sorted order with some out-of-order elements nearly in position. `
+const desc = `A simple sorting algorithm that repeatedly steps through the list, compares adjacent elements and swaps them if they are in the wrong order. The pass through the list is repeated until the list is sorted. The pass through the list is repeated until the list is sorted. The algorithm, which is a comparison sort, is named for the way smaller or larger elements "bubble" to the top of the list. Although the algorithm is simple, it is too slow and impractical for most problems even when compared to insertion sort. Bubble sort can be practical if the input is in mostly sorted order with some out-of-order elements nearly in position. `;
 
 export const data = {
   name,
   slug,
+  complexities,
   link,
   desc,
-  complexities,
-}
+};
 
 export default bubble_sort;
--- src/sorts/merge.js
+++ src/sorts/merge.js
@@ -4,35 +4,37 @@ function* merge_sort(A, { set }) {
   A = A.slice();
   const tdsm = TopDownSplitMerge(B, 0, A.length, A, set);
   for (let done = false; !done; { done } = tdsm.next()) {
-    yield
+    yield;
   }
 }
 
 function* TopDownSplitMerge(B, iBegin, iEnd, A, set) {
-  if (iEnd - iBegin < 2)                       // if run size == 1
-    return;                                 //   consider it sorted
+  if (iEnd - iBegin < 2)
+    // if run size == 1
+    return; //   consider it sorted
   // split the run longer than 1 item into halves
-  const iMiddle = (iEnd + iBegin) / 2 | 0;              // iMiddle = mid point
+  const iMiddle = ((iEnd + iBegin) / 2) | 0; // iMiddle = mid point
   // recursively sort both runs from array A[] into B[]
 
-  const tdsmLeft = TopDownSplitMerge(A, iBegin, iMiddle, B, set);  // sort the left  run
+  const tdsmLeft = TopDownSplitMerge(A, iBegin, iMiddle, B, set); // sort the left  run
   for (let done = false; !done; { done } = tdsmLeft.next()) {
-    yield
+    yield;
   }
-  const tdsmRight = TopDownSplitMerge(A, iMiddle, iEnd, B, set);;
+  const tdsmRight = TopDownSplitMerge(A, iMiddle, iEnd, B, set);
   // sort the right run
   for (let done = false; !done; { done } = tdsmRight.next()) {
-    yield
+    yield;
   }
   // merge the resulting runs from array B[] into A[]
   const tdm = TopDownMerge(B, iBegin, iMiddle, iEnd, A, set);
   for (let done = false; !done; { done } = tdm.next()) {
-    yield
+    yield;
   }
 }
 
 function* TopDownMerge(A, iBegin, iMiddle, iEnd, B, set) {
-  let i = iBegin, j = iMiddle;
+  let i = iBegin,
+    j = iMiddle;
   // While there are elements in the left or right runs...
   for (let k = iBegin; k < iEnd; k++) {
     // If left run head exists and is <= existing right run head.
@@ -50,25 +52,25 @@ function* TopDownMerge(A, iBegin, iMiddle, iEnd, B, set) {
   }
 }
 
-const name = 'Merge Sort'
-const slug = 'merge-sort'
+const name = 'Merge Sort';
+const slug = 'merge-sort';
 
 const complexities = {
   best: 'n log(n)',
   average: 'n log(n)',
   worst: 'n log(n)',
-}
+};
 
-const link = 'https://en.wikipedia.org/wiki/Merge_sort'
+const link = 'https://en.wikipedia.org/wiki/Merge_sort';
 
-const desc = `Merge sort is an efficient, general-purpose, comparison-based sorting algorithm. Most implementations produce a stable sort, which means that the order of equal elements is the same in the input and output. Merge sort is a divide and conquer algorithm which splits up the array into sorted sub-arrays and merges them together.`
+const desc = `Merge sort is an efficient, general-purpose, comparison-based sorting algorithm. Most implementations produce a stable sort, which means that the order of equal elements is the same in the input and output. Merge sort is a divide and conquer algorithm which splits up the array into sorted sub-arrays and merges them together.`;
 
-
-export default merge_sort;
 export const data = {
   name,
   slug,
+  complexities,
   link,
   desc,
-  complexities,
-}
+};
+
+export default merge_sort;
